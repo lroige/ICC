1c167d845962aee5b281660690d7a6f52af4cc36
LAURA ROIGE FOIX <lroigefo11.alumnes@ub052229.mat.ub.edu>
2017-10-17 10:06:01 +0200

    Segueixo amb l'entrega

--- algebra/prac1funs.c
+++ algebra/prac1funs.c
@@ -1,3 +1,5 @@
+/* COGNOM1: ROIGÉ  COGNOM2: FOIX  NOM: LAURA  DNI: 47873311F */
+
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
@@ -5,71 +7,10 @@
 
 void resTinf (int n, double **L, double *x, double *b);
 void resTsup (int n, double **U, double *x, double *b);
+int normaVec (int n, double *vec);//PREGUNTA: ES PODEN POSAR MÉS FUNCIONS A LA LLIBRERIA?????
+void prodMatVec (int n, int m, double **A, double *b);
+void prodMatMat (int n, int m, double **A, double **B);
 
-int main(){
-    int n, i, j, sum;
-    double **L, *b, *x, *y;
-
-    printf("Introdueix la dimensio de la matriu: ");
-    scanf("%d", &n);
-
-    
-    /*Inicialitzem matriu L i vectors b i x*/
-    
-    x = (double*)malloc(n*sizeof(double*));
-
-    y = (double*)malloc(n*sizeof(double*));
-    
-    L = (double**)malloc(n*sizeof(double*));
-    for (i = 0; i < n; i++){
-        L[i]= (double*)malloc(n*sizeof(double));
-    }
-
-    for (i = 0; i < n; i++){
-        for (j = 0; j < n; j++){
-            printf("Introduir l'element de la fila %d, columna %d de la matriu:\n", i+1, j+1);
-            scanf("%le", &L[i][j]);
-        }
-        if (L[i][i] != 1.){
-            print("L'element %d de la diagonal no es 1!");
-            return 1;
-        }
-    }
-    
-    b = (double*)malloc(n*sizeof(double*));
-    for (i = 0; i < n; i++){
-        printf("Introduir l'element %d del vector:\n", i+1);
-        scanf("%le", &b[i]);
-    }
-
-    resTinf(n, L, x, b);
-    
-    prodMatVec(n, n, L, x, y);
-    
-    sum = 0
-    for (i = 0; i < n; i++){
-        sum += (y[i] - b[i]);
-    }
-    
-    /*Imprimim el vector solucio i la norma |Lx-b|2*/
-    printf("El vector solucio es:\n");
-    for(i = 0; i < n; i++){
-        printf("%f    ", x[i]);
-    }
-    printf("\n");
-    
-    printf("La norma |Lx - b|2 es: %le", norma);
-
-    /*Alliberar matriu*/
-    for(i = 0; i < n; i++){
-        free(L[i]);
-    }
-    free(L);
-    free(b);
-    free(x);
-    
-    return 0;
-}
 
 /*Aquesta funcio resol un sistema en una matriu triangular inferior i guarda la solucio al vector x*/
 void resTinf (int n, double **L, double *x, double *b){
@@ -85,4 +26,46 @@ void resTinf (int n, double **L, double *x, double *b){
     }
     
     return 0;
+}
+
+/*Aquesta funcio resol un sistema en una matriu triangular superior i guarda la solucio al vector x*/
+void resTinf (int n, double **L, double *x, double *b){
+    int i, j, sum;
+    
+    x[n - 1] = b[n - 1];
+    for (i = 1; i < n; i++){
+        sum = 0;
+        for (j = 0; j < i; j++){
+            sum += L[i][j] * x[k];
+        }
+        x[i] = b[i] - sum;
+    }
+    
+    return 0;
+}
+
+/*Aquesta funcio calcula la norma euclidiana d'un vector i retorna el resultat*/
+int normaVec (int n, double *vec){
+    int sum = 0
+    for (i = 0; i < n; i++){
+        sum += (y[i] - b[i])²;
+    }
+    return sqrt(sum);
+}
+
+/*Aquesta funcio calcula el producte d'una matriu per un vector*/
+//REVISAR I AFEGIR LES CONSIDERACIONS DE M
+void prodMatVec (int n, int m, double **A, double *b){
+    int i, j;
+    
+    for(i = 0; i < n; i++){
+        resultat[i] = 0;
+    }
+    
+    for (i = 0; i < n; i++){
+        for (j = 0; j < n; j++){
+            resultat[i] += A[i][j] * b[j];
+        }
+    }
+    return 1;
 }
